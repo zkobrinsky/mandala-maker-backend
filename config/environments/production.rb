14a22b43b6233ea83ac9142cf89f48ed05fedac5
zkobrinsky <zachkobrinsky@gmail.com>
2021-01-05 16:32:55 -0500

    RAILS_ENV=production bundle exec rake assets:precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  RAILS_ENV=production bundle exec rake assets:precompile
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
