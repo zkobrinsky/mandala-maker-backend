d169f3061d46b3aa2d7532f17f17735a332e84d6
zkobrinsky <zachkobrinsky@gmail.com>
2021-01-05 16:29:42 -0500

    edited cors path

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://your-app-name.herokuapp.com'
+    origins 'https://mandala-maker-frontend.herokuapp.com'
 
     resource '*',
       headers: :any,
