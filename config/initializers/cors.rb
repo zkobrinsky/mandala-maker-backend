231dba4540e24ca6e465c3288497546ce3aa7e1f
zkobrinsky <zachkobrinsky@gmail.com>
2021-01-07 23:18:40 -0500

    changed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mandala-maker-frontend.herokuapp.com'
+    origins 'https://mandalas.herokuapp.com/'
 
     resource '*',
       headers: :any,
