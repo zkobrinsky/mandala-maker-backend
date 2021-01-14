5f7b290bcf71d5ff6b7f623e80485ac57f7b700d
zkobrinsky <zachkobrinsky@gmail.com>
2021-01-14 10:11:27 -0500

    removed trailing slash from cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mandalas.herokuapp.com/'
+    origins 'https://mandalas.herokuapp.com'
 
     resource '*',
       headers: :any,
